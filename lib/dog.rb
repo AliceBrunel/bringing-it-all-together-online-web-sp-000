fdd4de0e6a3574d03a4dfb1d2e817d67f4e1afa4
Alice Brunel <alc.brunel@gmail.com>
2019-09-18 08:49:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,12 +27,7 @@ class Dog
   end
   
   def save 
-    sql = <<-SQL
-      INSERT INTO dogs (name, breed) 
-      VALUES (?, ?)
-    SQL
-    DB[:conn].execute(sql, self.name, self.breed)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
   end
   
 end
