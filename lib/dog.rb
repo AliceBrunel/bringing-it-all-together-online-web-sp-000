356d4182fb0cec801a399155b6ae1b4a9eb8e729
Alice Brunel <alc.brunel@gmail.com>
2019-09-18 09:42:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -98,10 +98,11 @@ class Dog
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty? 
       dog_data = dog[0]
-      dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+      new_dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
-      dog = self.create(dog_data)
+      new_dog = self.create(dog_data)
     end
+    new_dog
   end
   
 end
